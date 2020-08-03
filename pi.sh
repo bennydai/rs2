bd05dc864db6f3fd8e79041ca570d7c4da9c631c
bennydai <benny.dai@uts.edu.au>
2020-08-03 19:42:24 +1000

    backip

--- pi.sh
+++ pi.sh
@@ -6,5 +6,5 @@ pi_ip=$(ping -c 1 $HOSTNAME | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p')
 echo Laptop IP is $laptop_ip
 echo Pi IP is $pi_ip
 
-docker-compose -f docker-compose.yaml run raspberrypi
+docker-compose -f docker-compose.yaml run raspberrypi  
 docker-compose -f docker-compose.yaml stop raspberrypi
